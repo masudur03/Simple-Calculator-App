88791cc39c8265a1b8bce7ce7449f57f1154826f
Masudur Rahman <monim@Masudurs-MacBook-Pro.local>
2024-01-30 16:49:10 -0800

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,85 @@
-import logo from './logo.svg';
-import './App.css';
+import {
+  useState,
+  useRef
+} from "react";
+import "./App.css";
 
 function App() {
+  const inputRef = useRef(null);
+  const resultRef = useRef(null);
+  const [result, setResult] = useState(0);
+
+  function plus(e) {
+    e.preventDefault();
+    setResult((result) => result + Number(inputRef.current.value));
+  };
+
+  function minus(e) {
+    // Add the code for the minus function 
+    e.preventDefault();
+    setResult((result) => result - Number(inputRef.current.value));
+  };
+
+  function times(e) {
+    // Add the code for the plus function 
+    e.preventDefault();
+    setResult((result) => result * Number(inputRef.current.value));
+  };
+
+  function divide(e) {
+    // Add the code for the divide function
+    e.preventDefault();
+    setResult((result) => result / Number(inputRef.current.value));
+  };
+
+  function resetInput(e) {
+    // Add the code for the resetInput function
+    e.preventDefault();
+    inputRef.current.value = "";
+
+  };
+
+  function resetResult(e) {
+    // Add the code for the resetResult function 
+    e.preventDefault();
+    setResult(0);
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
+      <div>
+        <h1>Simplest Working Calculator</h1>
+      </div>
+      <form>
+        <p ref={resultRef}>
+          {result}
+
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <input
+          pattern="[0-9]"
+          ref={inputRef}
+          type="number"
+          placeholder="Type a number"
+        />
+        <button onClick={plus}>add</button>
+        {/* Add the subtract button */}
+        <button onClick={minus}>subtract</button>
+
+        {/* Add the multiply button */}
+        <button onClick={times}>times</button>
+
+        {/* Add the divide button */}
+        <button onClick={divide}>divide</button>
+
+        {/* Add the resetInput button */}
+        <button onClick={resetInput}>reset input</button>
+
+        {/* Add the resetResult button */}
+        <button onClick={resetResult}>reset result</button>
+
+      </form>
     </div>
   );
 }
 
-export default App;
+export default App; 
